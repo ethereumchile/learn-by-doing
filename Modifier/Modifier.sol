2174f38cabc69155001a746f9cf6a907271ff099
EthereumChile <javier@ethereumchile.cl>
2021-12-09 05:54:51 +0100

    Replace tabs -> spaces

--- Modifier/Modifier.sol
+++ Modifier/Modifier.sol
@@ -4,22 +4,22 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract Modifier {
-	address public administrator;
-	uint256 public balance;
+    address public administrator;
+    uint256 public balance;
 
-	modifier checkConditionFirst() {
-		require(msg.sender == administrator, "You're not administrator");
-		_;
-	}
+    modifier checkConditionFirst() {
+        require(msg.sender == administrator, "You're not administrator");
+        _;
+    }
 
-	function addBalanceWithModifier(uint256 amount) public checkConditionFirst returns (uint256) {
-		balance = balance + amount;
-		return balance;
-	}
+    function addBalanceWithModifier(uint256 amount) public checkConditionFirst returns (uint256) {
+        balance = balance + amount;
+        return balance;
+    }
 
-	function addBalanceWithoutModifier(uint256 amount) public returns (uint256) {
-		require(msg.sender == administrator, "You're not administrator");
-		balance = balance + amount;
-		return balance;
-	}
+    function addBalanceWithoutModifier(uint256 amount) public returns (uint256) {
+        require(msg.sender == administrator, "You're not administrator");
+        balance = balance + amount;
+        return balance;
+    }
 }
